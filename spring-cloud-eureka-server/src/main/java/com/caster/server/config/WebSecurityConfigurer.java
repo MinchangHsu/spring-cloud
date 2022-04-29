6ef7df45b7c4a4f88714b614d0169755293bb4a5
caster <a4947887@gmail.com>
2022-04-29 16:27:45 +0800

    Eureka 增加 security - fail
    原本要增加這項設定
    但client端 就是會出現401 無法驗證成功
    問題似乎是 spring boot 較新的版本會有這問題
    目前也沒有查到解決方式，就連解決方式都沒查到
    看起來降板會是比較快的方式
    但也不確定是否可以解決
    這是個大坑.....

---
+++ spring-cloud-eureka-server/src/main/java/com/caster/server/config/WebSecurityConfigurer.java
@@ -0,0 +1,19 @@
+package com.caster.server.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Author: Minchang Hsu (Caster)
+ * Date: 2022/4/29
+ * Comment:
+ */
+//@EnableWebSecurity
+//public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//		http.csrf().ignoringAntMatchers("/eureka/**");
+//		super.configure(http);
+//	}
+//}
